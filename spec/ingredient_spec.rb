a9e2fd98a91276975aa41e35f88696ba6b87509b
Corrie Smith & Lisa Folliard <student@epicodus.com>
2015-08-26 11:08:38 -0700

    pass many to many spec to determine which recipe is used with which ingredients

--- spec/ingredient_spec.rb
+++ spec/ingredient_spec.rb
@@ -1,6 +1,18 @@
 require('spec_helper')
 
 describe(Ingredient) do
+  describe('#recipes') do
+    it('determines which recipe is used with which ingredient') do
+      new_recipe = Recipe.create({:name => 'penne vodka'})
+      new_ingredient = Ingredient.create({:description => 'pasta'})
+      new_ingredient2 = Ingredient.create({:description => 'tomatoes'})
+      new_ingredient3 = Ingredient.create({:description => 'basil'})
+      new_ingredient.recipes.push(new_recipe)
+      new_ingredient2.recipes.push(new_recipe)
+      expect(new_ingredient.recipes()).to(eq([new_recipe]))
+    end
+  end
+
   it("validates the presence of a description") do
     ingredient = Ingredient.create({:description => ""})
     expect(ingredient.save()).to(eq(false))
