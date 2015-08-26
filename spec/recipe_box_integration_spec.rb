0eb43a24bd11f30a69b4aa5427a1d7b94d316057
Corrie Smith & Lisa Folliard <student@epicodus.com>
2015-08-26 14:10:30 -0700

    add and display new ingredients

--- spec/recipe_box_integration_spec.rb
+++ spec/recipe_box_integration_spec.rb
@@ -8,3 +8,12 @@ describe('path to view individual recipe page', :type => :feature) do
     expect(page).to have_content('beef bourguignon')
   end
 end
+
+describe('path to view individual ingredient page', :type => :feature) do
+  it('adds and displays new ingredients') do
+    visit('/ingredients')
+    fill_in('description', :with => 'tempeh')
+    click_button('Add!')
+    expect(page).to have_content('tempeh')
+  end
+end
