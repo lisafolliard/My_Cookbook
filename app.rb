08986c0280fdf7b2a6b5b988172d1845b666e9dc
Corrie Smith & Lisa Folliard <student@epicodus.com>
2015-08-26 14:03:36 -0700

    add ability to view and add recipes and ingredients, all routing working

--- app.rb
+++ app.rb
@@ -5,5 +5,29 @@ Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 get('/') do
-erb(:index)
+  erb(:index)
+end
+
+get('/recipes') do
+  @recipes = Recipe.all()
+  erb(:recipes)
+end
+
+post('/recipes') do
+  name = params.fetch("name")
+  recipe = Recipe.create({:name => name})
+  @recipes = Recipe.all()
+  erb(:recipes)
+end
+
+get('/ingredients') do
+  @ingredients = Ingredient.all()
+  erb(:ingredients)
+end
+
+post('/ingredients') do
+  description = params.fetch('description')
+  ingredient = Ingredient.create({:description => description})
+  @ingredients = Ingredient.all()
+  erb(:ingredients)
 end
