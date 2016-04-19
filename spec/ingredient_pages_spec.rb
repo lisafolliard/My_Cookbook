847f4f2e64cab6f1295b333d6cc71762a32455c5
Lisa Folliard <lisaf37@gmail.com>
2016-04-18 20:28:06 -0400

    update ingredient pages and recipe pages specs

--- spec/ingredient_pages_spec.rb
+++ spec/ingredient_pages_spec.rb
@@ -4,7 +4,7 @@ describe('path to view individual ingredient page', :type => :feature) do
   it('adds and displays new ingredients') do
     visit('/ingredients')
     fill_in('description', :with => 'tempeh')
-    click_button('Add!')
+    click_button('Add')
     expect(page).to have_content('Tempeh')
   end
 end
--- spec/recipe_pages_spec.rb
+++ spec/recipe_pages_spec.rb
@@ -4,7 +4,7 @@ describe('path to view individual recipe page', :type => :feature) do
   it('adds and displays new recipes') do
     visit('/recipes')
     fill_in('name', :with => 'beef bourguignon')
-    click_button('Add!')
+    click_button('Add')
     expect(page).to have_content('Beef bourguignon')
   end
 end
